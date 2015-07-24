efe50a438d80b62a26245fa4226139ffc33aea0f
H. Chase Stevens <chase@chasestevens.com>
2015-07-24 08:52:21 +0100

    add tree.py

---
+++ genetic_programming/tree.py
@@ -0,0 +1,20 @@
+
+import random
+
+class Node(object):
+    def __init__(self, f, allowed_functions=None):
+        self.f = f
+        self.rtype = f.__rtype
+        allowed_children = f.allowed_children()
+        if allowed_functions is not None:
+            allowed_children = [[child for child in child_list if child in allowed_functions] for child_list in allowed_children]
+        assert all(allowed_children), "{} has a parameter that cannot be satisfied.".format(self.f.func_name)
+        self.children = [Node(random.choice(child_list)) for child_list in allowed_children]
+        self.num_children = len(self.children)
+
+    def evaluate(self):
+        return self.f(*[child.evaluate() for child in self.children])
+
+
+def build_tree(rtype, allowed_functions=None):
+    pass
