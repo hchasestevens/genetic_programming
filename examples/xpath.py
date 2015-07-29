18311f5f347a1a1480b7a1707547cbfee7af950c
H. Chase Stevens <chase@chasestevens.com>
2015-07-30 00:13:07 +0100

    xpath - define new types

--- examples/xpath.py
+++ examples/xpath.py
@@ -10,17 +10,23 @@ class Attribute:
  
 class ValidExpression:
     pass
- 
+
+class NodeName:
+    pass
+
+class AxisName:
+    pass
+
  
 @params(Element, Element)
 @rtype(Element)
 def child(parent_elem, child_elem):
     return '{}/{}'.format(parent_elem, child_elem)
  
-@params(Element, Element)
-@rtype(Element)
-def descendant(parent_elem, descendant_elem):
-    return '{}//{}'.format(parent_elem, descendant_elem)
+#@params(Element, Element)
+#@rtype(Element)
+#def descendant(parent_elem, descendant_elem):
+#    return '{}//{}'.format(parent_elem, descendant_elem)
  
 @params(str, str)
 @rtype(bool)
@@ -37,11 +43,6 @@ def xpath_and(cond1, cond2):
 def xpath_or(cond1, cond2):
     return '{} or {}'.format(cond1, cond2)
  
-#@params(Element)
-#@rtype(bool)
-#def condition_on_elem(elem):
-#    return elem
- 
 @params(int)
 @rtype(bool)
 def nonzero(n):
@@ -51,16 +52,21 @@ def nonzero(n):
 @rtype(str)
 def get_attribute(elem, attr):
     return '{}/@{}'.format(elem, attr)
- 
-@params()
+
+@params(AxisName, NodeName)
 @rtype(Element)
-def this():
-    return '.'
+def make_element(axis_name, node_name):
+    return '{}::{}'.format(axis_name, node_name)
  
-@params()
-@rtype(Element)
-def parent():
-    return '..'
+#@params()
+#@rtype(Element)
+#def this():
+#    return '.'
+ 
+#@params()
+#@rtype(Element)
+#def parent():
+#    return '..'
  
 @params(Element, bool)
 @rtype(Element)
@@ -86,11 +92,28 @@ def num_eq(num1, num2):
 @rtype(bool)
 def str_eq(s1, s2):
     return '{} = {}'.format(s1, s2)
+
+ancestor_axis = constant(AxisName, 'ancestor')
+ancestor_or_self_axis = constant(AxisName, 'ancestor-or-self')
+child_axis = constant(AxisName, 'child')
+descendant_axis = constant(AxisName, 'descendant')
+descendant_or_self_axis = constant(AxisName, 'descendant-or-self')
+following_axis = constant(AxisName, 'following')
+following_sibling_axis = constant(AxisName, 'following-sibling')
+parent_axis = constant(AxisName, 'parent')
+preceding_axis = constant(AxisName, 'preceding')
+preceding_sibling_axis = constant(AxisName, 'preceding-sibling')
+self_axis = constant(AxisName, 'self')
  
-@params()
-@rtype(Element)
-def wildcard():
-    return '*'
+# Some examples:
+wildcard = constant(NodeName, '*')
+div = constant(NodeName, 'div')
+form = constant(NodeName, 'form')
+p = constant(NodeName, 'p')
+span = constant(NodeName, 'span')
+
+name_attr = constant(Attribute, 'name')
+value_attr = constant(Attribute, 'value')
  
 @params(Element)
 @rtype(ValidExpression)
